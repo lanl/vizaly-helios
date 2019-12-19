81c6db493f5b9e25d7eb671a8b659cabd3dce0b6
Hoby Rakotoarivelo <hoby@lanl.gov>
2019-12-19 15:00:30 -0700

    Fix FFTW not found on Darwin

--- cmake/FindFFTW.cmake
+++ cmake/FindFFTW.cmake
@@ -10,19 +10,21 @@ if (FFTW_INCLUDES)
   set (FFTW_FIND_QUIETLY TRUE)
 endif (FFTW_INCLUDES)
 
-find_path(FFTW_INCLUDES fftw3.h)
-find_path(FFTWF_INCLUDES fftw3.f03)
+find_path(FFTW_INCLUDES fftw3.h
+        HINTS "/projects/opt/centos7/fftw/3.3.4/include")
 
-find_library (FFTW_LIB NAMES fftw3)
-find_library (FFTW_MPI NAMES fftw3_mpi)
-find_library (FFTWF_LIB NAMES fftw3f)
-#
-set(FFTW_LIBRARIES ${FFTWF_LIB} ${FFTW_LIB} ${FFTW_MPI})
-set(FFTWF_LIBRARIES ${FFTWF_LIB} ${FFTW_LIB})
+find_library(FFTW_LIB
+        NAMES libfftw3.a
+        PATHS "/projects/opt/centos7/fftw/3.3.4/lib")
+find_library(FFTW_MPI
+        NAMES libfftw3_mpi.a
+        PATHS "/projects/opt/centos7/fftw/3.3.4/lib")
+
+set(FFTW_LIBRARIES ${FFTW_LIB} ${FFTW_MPI})
 
 # handle the QUIETLY and REQUIRED arguments and set FFTW_FOUND to TRUE if
 # all listed variables are TRUE
 include (FindPackageHandleStandardArgs)
-find_package_handle_standard_args (FFTW DEFAULT_MSG FFTWF_LIBRARIES FFTW_LIBRARIES FFTWF_INCLUDES FFTW_INCLUDES)
+find_package_handle_standard_args(FFTW DEFAULT_MSG FFTW_LIBRARIES FFTW_INCLUDES)
 
-mark_as_advanced (FFTWF_LIBRARIES FFTW_LIBRARIES FFTWF_INCLUDES FFTW_INCLUDES )
+mark_as_advanced(FFTWF_LIBRARIES FFTW_LIBRARIES FFTW_INCLUDES)

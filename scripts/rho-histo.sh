f3c951423dae8a6b0567fac4e32624f9aa945af3
Hoby Rakotoarivelo <hoby@lanl.gov>
2020-01-27 14:53:49 -0700

    Create batch job script for Darwin

---
+++ scripts/rho-histo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --nodes 8
+#SBATCH --ntasks-per-node 1
+#SBATCH --partition scaling
+#SBATCH --job-name density
+
+RANKS=8
+
+BUILD="/projects/exasky/hoby-projects/helios/build"
+INPUT_JSON="../inputs/density_darwin.json"
+
+source "/home/hoby/.bashrc" && cd ${BUILD} &&
+mpirun -np ${RANKS} ./density ${INPUT_JSON}

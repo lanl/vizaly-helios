d23518ee4eccd97798c0a85a0a4c3acfce2061c2
Hoby Rakotoarivelo <hoby@lanl.gov>
2020-01-29 15:13:06 -0700

    Add script for power spectrum

---
+++ scripts/pk-rho.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --nodes 8
+#SBATCH --ntasks-per-node 1
+#SBATCH --partition scaling
+#SBATCH --job-name spectrum
+
+HACC="/projects/exasky/HACC"
+POWER_SPECTRUM="${HACC}/trunk/Darwin/mpi/bin/hacc_pk_gio_auto"
+PARTICLES_DATA="/home/hoby/dev/exasky/data/decompressed.gio"
+OUTPUT_DATA="/home/hoby/dev/exasky/data/pk-decomp.dat"
+TIMESTEP=499
+
+# shellcheck disable=SC1090
+source "${HACC}.darwin_setup" && cd "${HACC}/run" &&
+mpirun ${POWER_SPECTRUM} inputs/indat.params -n ${PARTICLES_DATA} ${OUTPUT_DATA} ${TIMESTEP}

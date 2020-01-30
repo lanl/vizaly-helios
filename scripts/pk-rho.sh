a832a8185f69c12e04b5963bb76eca231318ce70
Hoby Rakotoarivelo <hoby@lanl.gov>
2020-01-29 17:38:57 -0700

    Update input and output for pk computation

--- scripts/pk-rho.sh
+++ scripts/pk-rho.sh
@@ -7,8 +7,8 @@
 
 HACC="/projects/exasky/HACC"
 POWER_SPECTRUM="${HACC}/trunk/Darwin/mpi/bin/hacc_pk_gio_auto"
-PARTICLES_DATA="/home/hoby/dev/exasky/data/decompressed.gio"
-OUTPUT_DATA="/home/hoby/dev/exasky/data/pk-decomp.dat"
+PARTICLES_DATA="/home/hoby/dev/exasky/data/decompressed_two.gio"
+OUTPUT_DATA="/home/hoby/dev/exasky/data/pk-decomp_two.dat"
 TIMESTEP=499
 
 # shellcheck disable=SC1090

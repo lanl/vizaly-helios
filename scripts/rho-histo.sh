b82b0327c687fa549afa6a2dd4a1aadd98e9d20c
Hoby Rakotoarivelo <hoby@lanl.gov>
2020-02-13 16:04:33 -0700

    Add current timestep

--- scripts/rho-histo.sh
+++ scripts/rho-histo.sh
@@ -6,8 +6,9 @@
 #SBATCH --job-name density
 
 ranks=8
+timestep=272
 build="/projects/exasky/hoby-projects/helios/build"
-input_json="../inputs/density_darwin.json"
+input_json="../inputs/density_darwin_${timestep}.json"
 
 source "/home/hoby/.bashrc" && cd ${build} &&
 mpirun -np ${ranks} ./density ${input_json}
